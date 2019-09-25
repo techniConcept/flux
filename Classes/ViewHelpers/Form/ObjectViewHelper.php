e45e01886bd3027f1776352c23d9a00dc3e9d118
Julien Fourel <julien.fourel@techniconcept.ch>
2019-09-25 14:10:05 +0200

    TASK: Compatibility for PHP 7.2+

--- Classes/ViewHelpers/Form/ObjectViewHelper.php
+++ Classes/ViewHelpers/Form/ObjectViewHelper.php
@@ -8,7 +8,7 @@ namespace FluidTYPO3\Flux\ViewHelpers\Form;
  * LICENSE.md file that was distributed with this source code.
  */
 
-use FluidTYPO3\Flux\Form\Container\Object as ObjectComponent;
+use FluidTYPO3\Flux\Form\Container\FluxObject as ObjectComponent;
 use FluidTYPO3\Flux\ViewHelpers\AbstractFormViewHelper;
 use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
 
@@ -87,7 +87,7 @@ class ObjectViewHelper extends AbstractFormViewHelper
     {
         /** @var ObjectComponent $object */
         $object = static::getFormFromRenderingContext($renderingContext)
-            ->createContainer('Object', $arguments['name'], $arguments['label']);
+            ->createContainer('FluxObject', $arguments['name'], $arguments['label']);
         $object->setExtensionName(
             static::getExtensionNameFromRenderingContextOrArguments($renderingContext, $arguments)
         );
